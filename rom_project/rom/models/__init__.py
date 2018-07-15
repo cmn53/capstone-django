bb304e783f1c406148721ddf436dffbb0c76a36b
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-07-15 00:12:00 -0700

    Created pattern model

--- rom_project/rom/models/__init__.py
+++ rom_project/rom/models/__init__.py
@@ -3,5 +3,5 @@ from .hotel import Hotel
 from .image import Image
 from .operator import Operator
 from .route import Route
-# from .pattern import Pattern
+from .pattern import Pattern
 # from .stop import Stop
---
+++ rom_project/rom/models/pattern.py
@@ -0,0 +1,21 @@
+from django.contrib.gis.db import models
+from .route import Route
+
+class Pattern(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    pattern_onestop_id = models.CharField(max_length=100)
+    wk_trips = models.IntegerField()
+    sa_trips = models.IntegerField()
+    su_trips = models.IntegerField()
+    wk_00_03 = models.IntegerField()
+    wk_03_06 = models.IntegerField()
+    wk_06_09 = models.IntegerField()
+    wk_09_12 = models.IntegerField()
+    wk_12_15 = models.IntegerField()
+    wk_15_18 = models.IntegerField()
+    wk_18_21 = models.IntegerField()
+    wk_21_24 = models.IntegerField()
+    wk_24_28 = models.IntegerField()
+
+    def __str__(self):
+        return "%s Pattern %s" %(self.route.operator.name, self.pattern_onestop_id)
