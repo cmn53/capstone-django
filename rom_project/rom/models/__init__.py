847be59c96c802bfeb8c07ff182bfd9aa85cc5c9
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-07-14 23:11:43 -0700

    Create route model

--- rom_project/rom/models/__init__.py
+++ rom_project/rom/models/__init__.py
@@ -2,6 +2,6 @@ from .metro import Metro
 from .hotel import Hotel
 from .image import Image
 from .operator import Operator
-# from .route import Route
+from .route import Route
 # from .pattern import Pattern
 # from .stop import Stop
