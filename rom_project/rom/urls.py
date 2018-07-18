30cd339ae88d532039e4204133a7b6c9726578f2
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-07-18 12:55:38 -0700

    Added basic views and templates

---
+++ rom_project/rom/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'rom'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('results/<metro_id>/', views.results, name='results'),
+    path('hotel/<int:hotel_id>/', views.detail, name='detail'),
+]
--- rom_project/rom/views.py
+++ rom_project/rom/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+from .models import Hotel, Metro, Score
+
+def index(request):
+    hotels = Hotel.objects.all()
+    context = {'hotels': hotels}
+    return render(request, 'rom/index.html', context)
+
+def detail(request, hotel_id):
+    hotel = get_object_or_404(Hotel, pk=hotel_id)
+    return render(request, 'rom/detail.html', {'hotel': hotel})
+
+def results(request, metro_id):
+    metro = get_object_or_404(Metro, pk=metro_id)
+    hotels = Hotel.objects.filter(metro=metro)
+    context = {'metro': metro, 'hotels': hotels}
+    return render(request, 'rom/results.html', context)
