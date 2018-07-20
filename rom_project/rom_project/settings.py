bc255a095a9eb69040f40fc97c45d0bb1266869d
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-07-19 23:55:18 -0700

    Some new settings on static files

--- rom_project/rom_project/settings.py
+++ rom_project/rom_project/settings.py
@@ -27,9 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = [
-    '0.0.0.0',
     'localhost',
-    'rom-free.herokuapp.com',
+    'www.rom-free.herokuapp.com',
 ]
 
 
@@ -127,14 +126,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
